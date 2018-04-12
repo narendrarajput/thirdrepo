cdcb9d6c68f6ad8cc93734711e53344f670e8ec2
narendra1989 <narendrarajput3004@gmail.com>
2018-04-12 17:20:03 +0530

    print statement added

--- src/com/automation/AutomatedTest.java
+++ src/com/automation/AutomatedTest.java
@@ -3,6 +3,7 @@ package com.automation;
 public class AutomatedTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Automation");
+		System.out.println("another message to checkout");
 	}
 
 }
