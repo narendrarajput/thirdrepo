4d22cbdb6526896338d4b2d1ccfa2624f21c7f1b
narendra1989 <narendrarajput3004@gmail.com>
2018-05-25 11:54:39 +0530

    test line added in AutomatedTest.java class

--- src/com/automation/AutomatedTest.java
+++ src/com/automation/AutomatedTest.java
@@ -4,6 +4,7 @@ public class AutomatedTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Automation");
 		System.out.println("another message to checkout");
+		System.out.println("Simple svn example");
 	}
 
 }
